6b6a43b47d70e669b36fc85215e8da0473ce43d5
burak <burak@BURAK>
2024-10-16 11:03:09 +0300

    Add project files.

---
+++ Context/PatikaSecondDbContext.cs
@@ -0,0 +1,30 @@
+﻿using CodeFirstRelation2.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstRelation2.Context
+{
+    public class PatikaSecondDbContext : DbContext
+    {
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=BURAK\SQLEXPRESS; Database=PatikaCodeFirstDb2; TrustServerCertificate=true; trusted_connection=true;");
+            base.OnConfiguring(optionsBuilder);
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<UserEntity>()
+                                .HasOne(u => u.UserPost)
+                                .WithMany(p => p.Posts)
+                                .HasForeignKey(p => p.UserPostId);
+                                
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<PostEntity> Posts => Set<PostEntity>();
+        public DbSet<UserEntity> Users => Set<UserEntity>();
+    }
+}
---
+++ Entities/PostEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeFirstRelation2.Entities
+{
+    public class PostEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int UserId { get; set; }
+
+        public PostEntity UserPost { get; set; }
+    }
+}
